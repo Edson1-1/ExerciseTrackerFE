f24c5d23753463f8603db0f1305cefe3c78d0b43
Edson1-1 <edson.reginol98@gmail.com>
2020-09-20 23:15:24 +0530

    ExerciseTrackerApp

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import Navbar from "./components/navbarComponent";
+import CreateExercise from  "./components/createExerciseComponent";
+import CreateUser from "./components/createUserComponent";
+import EditExercise from "./components/editExerciseComponent";
+import ExercisesList from "./components/exerciseListComponent";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
+  );
+ }
+  
+
+export default App;
